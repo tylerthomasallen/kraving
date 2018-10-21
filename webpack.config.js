a899654fd3298a82f2b7e4e880d916894182a4e9
Tyler <tylerthomasallen@gmail.com>
2018-10-21 16:56:07 -0700

    Get localhost working

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/kraving',
+  entry: './frontend/entry',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
