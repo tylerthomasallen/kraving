1a188a710ba5e90b25dc0f53ae0116baa031ab7e
Tyler <tylerthomasallen@gmail.com>
2018-10-21 17:58:25 -0700

    Get splash comp rendering

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import LoginContainer from './session/login_container';
 import SignUpContainer from './session/signup_container';
+import SplashIndex from './splash/splash_index';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Switch>
         <Route exact path="/login" component={LoginContainer} />
         <Route exact path="/signup" component={SignUpContainer} />
-        <Redirect to="/"/>
+        <Route exact path="/" component={SplashIndex} />
       </Switch>
     </div>
   );
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,14 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
-
-  // TESTING START
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  // TESTING END
+  debugger;
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
